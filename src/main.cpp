4cea3a58b220de811a5dab810954afadb3b45c2a
davorl <davide@davideorlandi.it>
2020-05-13 21:34:12 +0200

    Temporary test movement in main().

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,30 @@ void setup()
 }
 
 
+void loop()
+{
+  float a = (millis() % (int)(PI*1000)) * 2.0 / 1000.0 * 2.0;
+  float x = cos(a) * 30.0;
+  float y = sin(a) * 30.0;
+  float z = -85.0;
+  for (uint8_t i = 0; i<NUM_LEGS; i++)
+  {
+    Leg *l = &ragnetto.legs[i];
+    Point3d p(
+      x + cos(l->attachmentAngle)*(LEG_SEGMENT_1_LENGTH + LEG_SEGMENT_2_LENGTH/3+30),
+      y+sin(l->attachmentAngle)*(LEG_SEGMENT_1_LENGTH + LEG_SEGMENT_2_LENGTH/3+30),
+      z);
+    l->moveTo(p);
+  }
+  delay(10);
+}
+
+/*
+
 #define STEP 5
+
 Point3d p(-50.0, 0.0, -90.0);
+
 void loop()
 {
   if (Serial && Serial.available())
@@ -37,3 +59,4 @@ void loop()
 
   }
 }
+*/
