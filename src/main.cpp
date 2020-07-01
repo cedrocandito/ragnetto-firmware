cf962625b7800ee65c5d8807d492c6c122128838
davorl <davide@davideorlandi.it>
2020-07-01 23:17:49 +0200

    Initialize Serial only if needed (uses a lot of memory).

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,12 @@ Ragnetto ragnetto;
 
 void setup()
 {
+  #ifndef BLUETOOTH_SERIAL 
   /* For some reason the serial port must be initialized immediately or else it will not work. */
   Serial.begin(HARDWARE_SERIAL_BAUDRATE);
   delay(300);
+  #endif
+
   setup_hardware();
   configuration.read();
   ragnetto_serial.send_info(F("Ragnetto is ready."));
